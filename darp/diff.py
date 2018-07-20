47a202339ef8be433f572284ddf0156f6ef753d0
derwentx <derwentx@gmail.com>
2018-07-20 19:51:34 +1000

    reduce complexity of mac_difference

--- darp/diff.py
+++ darp/diff.py
@@ -3,14 +3,22 @@ class ScanDiff(object):
         self.old_scan = old_scan
         self.new_scan = new_scan
 
+    @property
+    def new_macs(self):
+        return [scan.get('mac') for scan in self.new_scan]
+
+    @property
+    def old_macs(self):
+        return [scan.get('mac') for scan in self.old_scan]
+
     def mac_difference(self):
         added = []
         removed = []
-        new_macs = [scan.get('mac') for scan in self.new_scan]
+        new_macs = self.new_macs
         if not self.old_scan:
             added = new_macs
         else:
-            old_macs = [scan.get('mac') for scan in self.old_scan]
+            old_macs = self.old_macs
             for mac in new_macs:
                 if mac not in old_macs:
                     added.append(mac)
